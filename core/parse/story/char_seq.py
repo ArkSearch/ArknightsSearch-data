001c4b8c73982e7bbaea7a029c2ced32cc1db503
meeboo3 <meeboo3@qq.com>
2023-07-20 06:19:12 +0800

    feat 适配同id char,avg

---
+++ core/parse/story/char_seq.py
@@ -0,0 +1,44 @@
+__all__ = ['seq_data']
+
+import re
+import asyncio
+
+import simdjson as json
+import aiohttp
+
+r1 = re.compile('^\d')
+
+
+def get_seq(char_id: str) -> str:
+    group = char_id.split('_')
+    for i, seq in enumerate(group):
+        if r1.search(seq):
+            break
+    else:
+        raise ValueError('Cannot get seq from %s' % group)
+    return 'npc_' + seq if 'npc' in group else seq + ('_' + group[i + 1] if i + 1 < len(group) else '')
+
+
+async def get_data():
+    async with aiohttp.ClientSession() as client:
+        async with client.get('https://raw.githubusercontent.com/Arkfans/ArknightsName/main/data/npc.json') as r:
+            return json.loads(await r.text())
+
+
+name_data: dict[str, dict[str, list[str]]] = asyncio.new_event_loop().run_until_complete(get_data())
+pre_seq_data: dict[str, dict[str, set[str]]] = {}
+seq_data: list[list[list[str]]] = []
+
+for char_id, char_name in name_data.items():
+    char_seq = get_seq(char_id)
+    if char_seq not in pre_seq_data:
+        pre_seq_data[char_seq] = {'id': {char_id}, 'name': set()}
+    else:
+        pre_seq_data[char_seq]['id'].add(char_id)
+
+    for lang, names in char_name.items():
+        for name in names:
+            pre_seq_data[char_seq]['name'].add(name)
+
+for _, char_data in sorted(pre_seq_data.items(), key=lambda x: x[0]):
+    seq_data.append([(sorted(char_data['id'])), sorted(char_data['name'])])
--- core/parse/story/save.py
+++ core/parse/story/save.py
@@ -7,7 +7,7 @@ from .zone_name import *
 from .text_index import *
 from .char_index import *
 from .zone_index import *
-from .char_name import *
+from .char_seq import *
 
 story_data = sort_dict(story_data)
 text_data = sort_dict(text_data, 1)
@@ -16,8 +16,6 @@ text_index = sort_dict(text_index)
 char_id2story = sort_dict(char_id2story)
 char_name2story = sort_dict(char_name2story)
 zone_index = sort_dict(zone_index)
-char_id2name = sort_dict(char_id2name)
-char_name2id = sort_dict(char_name2id)
 
 # 基础数据
 save('story_data', {i[0]: i[1].dump for i in story_data.items()})
@@ -30,6 +28,5 @@ save('char_id2story', char_id2story)
 save('char_name2story', char_name2story)
 # zone索引
 save('zone_index', zone_index)
-# char name数据
-save('char_name2id', char_name2id)
-save('char_id2name', char_id2name)
+# char name&id 源数据
+save('seq_data', seq_data)
